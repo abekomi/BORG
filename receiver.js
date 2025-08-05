bdf82de9347bd7cf11519983f4736538ae35526b
abekomi <akomirox@gmail.com>
2025-08-05 12:06:59 -0400

    added group id in reciver.js

--- receiver.js
+++ receiver.js
@@ -1,14 +1,18 @@
-import { app } from "./index.mjs";
+import { app } from "./index.js";
 import { commandsLookup } from "./commands.js";
 
-export function receiver(token) {
-	app.post("/" + token, function (req, resp) {
-		let command = req.body.text.split(" ");
-		if (command[0] === "BORG" && command[1] in commandsLookup) {
-			commandsLookup[command[1]]({
-				token: token,
-				gid: req.body.group_id,
-			});
-		}
-	});
+export function receiver(token, botId) {
+  app.post("/" + token, function (req, res) {
+    const command = req.body.text.split(" ");
+
+    if (command[0] === "BORG" && command[1] in commandsLookup) {
+      commandsLookup[command[1]].call(command.slice(2).join(" "), {
+        token: token,
+        bot_id: botId,
+        gid: req.body.group_id
+      });
+    }
+
+    res.sendStatus(200);
+  });
 }
