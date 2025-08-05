2860490c0bd264d3eb57e9eab7520521eff3f8a2
abekomi <akomirox@gmail.com>
2025-08-05 12:59:54 -0400

    index.mjs

--- index.mjs
+++ index.mjs
@@ -1,5 +1,3 @@
-// index.mjs
-
 console.log('Running Node version:', process.version);
 
 import { getData } from './utils.js';
@@ -7,15 +5,18 @@ import { receiver } from './receiver.js';
 import express from 'express';
 import bodyParser from 'body-parser';
 
-export const app = express();  // export here
+export const app = express();
 app.use(bodyParser.json());
 
+const BOT_ID = process.env.BOT_ID;  // get bot id from env vars
+
 getData((data) => {
-	for (let i in data.clients) {
-		receiver(data.clients[i].token);
-	}
+  for (let i in data.clients) {
+    // Pass both token and BOT_ID to receiver
+    receiver(data.clients[i].token, BOT_ID);
+  }
 }, "data");
 
 app.listen(3000, () => {
-	console.log("Server running on port 3000");
+  console.log("Server running on port 3000");
 });
