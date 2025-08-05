b6dacb0a23e3d111b77573917537e400b3c417aa
abekomi <akomirox@gmail.com>
2025-08-05 08:58:32 -0400

    fixed up receiver.js

--- receiver.js
+++ receiver.js
@@ -1,13 +1,13 @@
-import {app} from "./index.js"
-import {commandsLookup} from "./commands";
+import { app } from "./index.js";
+import { commandsLookup } from "./commands.js";
 
 export function receiver(token) {
-	app.post("/" + token, function(req, resp) {
+	app.post("/" + token, function (req, resp) {
 		let command = req.body.text.split(" ");
-		if(command[0] === "BORG" && command[1] in commandsLookup) {
-			commandsLookup[command[1]].call(command.slice(0, 1).join(" "), {
+		if (command[0] === "BORG" && command[1] in commandsLookup) {
+			commandsLookup[command[1]]({
 				token: token,
-				gid: req.body.group_id
+				gid: req.body.group_id,
 			});
 		}
 	});
