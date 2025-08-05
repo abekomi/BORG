5badb741803a6d2621b585b65fc5ab722d8380a1
abekomi <akomirox@gmail.com>
2025-08-05 08:55:50 -0400

    added .js

--- index.mjs
+++ index.mjs
@@ -1,16 +1,21 @@
-console.log('Running Node version:', process.version);
-import {getData} from "./utils";
-import {receiver} from "./receiver";
+// index.mjs
 
-let express = require("express");
-let bodyParser = require("body-parser");
-export const app = express();
+console.log('Running Node version:', process.version);
+
+import { getData } from './utils.js';
+import { receiver } from './receiver.js';
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
 app.use(bodyParser.json());
 
 getData((data) => {
-	for(let i in data.clients) {
+	for (let i in data.clients) {
 		receiver(data.clients[i].token);
 	}
 }, "data");
 
-app.listen(3000);
+app.listen(3000, () => {
+	console.log("Server running on port 3000");
+});
